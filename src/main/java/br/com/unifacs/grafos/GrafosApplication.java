d05f7f658db24206a3b92e9c372f750078b7f03b
Matheus Ribeiro Garcia <matheus_ribeiro.g@hotmail.com>
2019-09-08 12:39:14 -0300

    Initial commit

---
+++ src/main/java/br/com/unifacs/grafos/GrafosApplication.java
@@ -0,0 +1,13 @@
+package br.com.unifacs.grafos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrafosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrafosApplication.class, args);
+	}
+
+}

ef70b734b9a6afe85e724766fb38c6bcc99162db
Matheus Ribeiro Garcia <matheus_ribeiro.g@hotmail.com>
2019-09-08 21:40:38 -0300

    Added menu runner

---
+++ src/main/java/br/com/unifacs/grafos/Menu.java
@@ -0,0 +1,13 @@
+package br.com.unifacs.grafos;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Menu implements CommandLineRunner {
+
+  @Override
+  public void run(String... args) {
+
+  }
+}
